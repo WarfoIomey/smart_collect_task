ebafb6969389ea99ce2e25b50b4e217e79423597
Kirill Turochkin <kirill180104@mail.ru>
2025-12-05 20:30:19 +0800

    fix flake8

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -37,11 +37,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
     path(
-        'swagger(<format>\.json|\.yaml)',
-        schema_view.without_ui(cache_timeout=0),
-        name='schema-json'
-    ),
-    path(
         'swagger/',
         schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'
